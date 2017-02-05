bab6879b7d54ef6d261ea3bc43127b4a52282844
Muhammad Ansharullah <ansorkazama@gmail.com>
2017-02-05 07:50:50 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
-# Define display_board that accepts a board and prints
+  # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  cols = "|"
+  sep_lines = "-----------"
+  puts " #{board[0]} #{cols} #{board[1]} #{cols} #{board[2]} "
+  puts "#{sep_lines}"
+  puts " #{board[3]} #{cols} #{board[4]} #{cols} #{board[5]} "
+  puts "#{sep_lines}"
+  puts " #{board[6]} #{cols} #{board[7]} #{cols} #{board[8]} "
+end 
